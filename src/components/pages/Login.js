13f3ea0989eb8c8d369eaca8031cfcf8f1344f9f
Cedric Coloma <cedcraftscodes@gmail.com>
2019-06-22 11:49:16 +0800

    Added Router and Login for Design

---
+++ src/components/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import LoginForm from '../auth/login/LoginForm';
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <LoginForm></LoginForm>
+            </div>
+        )
+    }
+}
+
+export default Login ;
