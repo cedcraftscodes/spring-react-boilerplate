4020a4156cde9e033ff1a72abca906bf4aa2bd2a
Cedric Coloma <cedcraftscodes@gmail.com>
2019-06-23 19:09:26 +0800

    Change route to /admin. Removed Register

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -25,7 +25,6 @@ function login(username, password) {
 }
 
 function logout() {
-    // remove user from local storage to log user out
     localStorage.removeItem('user');
 }
 
@@ -82,10 +81,7 @@ function handleResponse(response) {
         const data = text && JSON.parse(text);
         if (!response.ok) {
             if (response.status === 401) {
-                // auto logout if 401 response returned from api
                 logout();
-                // eslint-disable-next-line no-restricted-globals
-                location.reload(true);
             }
 
             const error = (data && data.message) || response.statusText;
