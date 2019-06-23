cd4f6c730292457a8896e022ac421591052c9969
Cedric Coloma <cedcraftscodes@gmail.com>
2019-06-23 16:06:15 +0800

    Modified Authentication entrypoint message

--- src/main/java/com/example/security/jwt/JwtAuthEntryPoint.java
+++ src/main/java/com/example/security/jwt/JwtAuthEntryPoint.java
@@ -1,11 +1,15 @@
 package com.example.security.jwt;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
@@ -23,7 +27,19 @@ public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
                          AuthenticationException e)
             throws IOException, ServletException {
 
-        logger.error("Unauthorized error. Message - {}", e.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error -> Unauthorized");
+//        logger.error("Unauthorized error. Message - {}", e.getMessage());
+//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final Map<String, Object> mapBodyException = new HashMap<>() ;
+        mapBodyException.put("message"  , e.getMessage()) ;
+        mapBodyException.put("path"     , request.getServletPath()) ;
+        mapBodyException.put("timestamp", (new Date()).getTime()) ;
+
+        response.setContentType("application/json") ;
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED) ;
+
+        final ObjectMapper mapper = new ObjectMapper() ;
+        mapper.writeValue(response.getOutputStream(), mapBodyException) ;
+
     }
 }
