f2ca12a737fad251efbd3710de71d377d7d5b275
Cedric Coloma <cedcraftscodes@gmail.com>
2019-06-24 20:52:54 +0800

    Separated User Insert and User Role

--- src/main/resources/db/migration/V3__UserInsert.sql
+++ src/main/resources/db/migration/V3__UserInsert.sql
@@ -1,4 +1,2 @@
 INSERT INTO `users` (`id`, `email`, `name`, `password`, `username`) VALUES
 (1, 'testtest@gmail.com', 'Cedric Coloma', '$2a$10$Uj0hOMPihvGT7XHsSfC5s.qykg8EjmTgNaZKE5RicccDzbX3Pqefm', 'testtest');
-INSERT INTO `user_roles` (`user_id`, `role_id`) VALUES ('1', '1');
-INSERT INTO `user_roles` (`user_id`, `role_id`) VALUES ('1', '2');
