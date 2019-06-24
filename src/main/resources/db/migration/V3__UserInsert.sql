e111710f0566632e0b40314c8910418afbc07363
Cedric Coloma <cedcraftscodes@gmail.com>
2019-06-24 21:01:17 +0800

    Attempt to fix Flyway Migration not accepting tilde

--- src/main/resources/db/migration/V3__UserInsert.sql
+++ src/main/resources/db/migration/V3__UserInsert.sql
@@ -1,2 +1,2 @@
-INSERT INTO `users` (`id`, `email`, `name`, `password`, `username`) VALUES
+INSERT INTO users (id, email, name, password, username) VALUES
 (1, 'testtest@gmail.com', 'Cedric Coloma', '$2a$10$Uj0hOMPihvGT7XHsSfC5s.qykg8EjmTgNaZKE5RicccDzbX3Pqefm', 'testtest');
